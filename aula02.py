29dd2ce09c098cb7f48ed4fac76df20942275cb9
GabrielFabiani <gabrielfabiani150@gmail.com>
2025-08-21 22:24:34 -0300

    Aula02- 21/08/2025

--- aula02.py
+++ aula02.py
@@ -1,5 +1,10 @@
-# Exercício 01
+# # Exercício 01
 Altura = float(input("Informe a sua altura: "))
 PesoI = (72.7 * Altura) - 58
 print(f"Seu peso ideal é: {PesoI:.2f}")
 
+# Exercício 02
+KM = float(input("Digite a quantidade de kilômetros rodados: "))
+Dia = int(input("Digite a quantidade de dias de aluguel: "))
+Total = (60 * Dia)+(KM * 0.15)
+print(f"Total a pagar: {Total:.2f}")
