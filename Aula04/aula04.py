25116e212f63c56058350a8e5c376a5f81e41d81
GabrielFabiani <gabrielfabiani150@gmail.com>
2025-09-04 21:42:50 -0300

    Aula dia 04/09/2025

---
+++ Aula04/aula04.py
@@ -0,0 +1,13 @@
+a = int(input("Lado a: "))
+b = int(input("Lado b: "))
+c = int(input("Lado c: "))
+if (a < b + c) and (b < a + c) and (c < a + b):
+     if (a == b) and ( b == c):
+       print("Triângulo equilátero")
+     else:
+         if (a == b) or (a == c) or (b == c):
+          print("Triângulo is isóceles")
+         else:
+          print("Triângulo escaleno")
+else:
+ print("Não é Triângulo")
