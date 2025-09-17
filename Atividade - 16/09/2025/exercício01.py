bc90a19b42f99dc19f39d99d1d2357041e29d5b0
GabrielFabiani <gabrielfabiani150@gmail.com>
2025-09-16 22:28:50 -0300

    atividade

--- Atividade - 16/09/2025/exercício01.py
+++ Atividade - 16/09/2025/exercício01.py
@@ -1,2 +1,27 @@
-num = int(input("Digite u número: "))
 
+# Exercício 01
+# num = int(input("Digite um número: "))
+
+# if num % 5 ==0: #verifica se é divisivel por 5
+#         print("Múltiplo de 5 detectado")
+
+
+# Exercício 02
+# altura = float(input("Digite sua altura em centímetros: "))
+
+# if altura>=150:
+#     print("Você tem altura  suficiente para entrar no Brinquedo")
+# else:
+#     print("Desculpe, você não tem altura suficiente para entrar no brinquedo")
+   
+# Exercício 03
+# senha = int(input("Digite uma senha: "))
+# if senha==1234:
+#     print("Acesso permitido")
+#     acesso = input("Deseja acessar a área administrativa('S' para Sim e 'N' para Não): ")
+#     if acesso=="S":
+#         print("Você está na área administrativa")
+#     else:
+#         print("Você está na área de usuário comum")
+# else:
+#     print("Senha incorreta! Acesso negado")
