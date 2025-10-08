17142cfc5cd09ccc0c18c14bc6a0c6a3bbd3ef41
GabrielFabiani <gabrielfabiani150@gmail.com>
2025-10-07 21:02:38 -0300

    Aula 07 - Funções

--- Aula07/exercicio07.py
+++ Aula07/exercicio07.py
@@ -1,21 +1,21 @@
-sexo = input("Digite M para masculino e F para feminino: ")
-peso = float(input("Digite seu peso em kg: "))
-def doar():
-    global sexo,peso
-    if sexo == "M" and peso>=60:
-        print("Você é homem e está apto a doar sangue")
-    elif sexo=="F" and peso>=50:
-        print("Você é mulher e está apta a doar sangue")
-    elif sexo == "M" and peso<60:
-        print("Você é homem e não está apto a doar sangue")
-    elif sexo == "M" and peso<50:
-        print("Você é mulher e não está apta a doar sangue")
+# sexo = input("Digite M para masculino e F para feminino: ")
+# peso = float(input("Digite seu peso em kg: "))
+# def doar():
+#     global sexo,peso
+#     if sexo == "M" and peso>=60:
+#         print("Você é homem e está apto a doar sangue")
+#     elif sexo=="F" and peso>=50:
+#         print("Você é mulher e está apta a doar sangue")
+#     elif sexo == "M" and peso<60:
+#         print("Você é homem e não está apto a doar sangue")
+#     elif sexo == "M" and peso<50:
+#         print("Você é mulher e não está apta a doar sangue")
 
 
-if sexo == "M" and peso>=60:
-        print("Você é homem e está apto a doa")
-elif sexo=="F" and peso>=50:
-        print("Você é mulher e está apta a doar sangue")
+# if sexo == "M" and peso>=60:
+#         print("Você é homem e está apto a doa")
+# elif sexo=="F" and peso>=50:
+#         print("Você é mulher e está apta a doar sangue")
 
-print 
+# print 
 
